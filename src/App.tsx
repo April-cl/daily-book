770c52792a13350080144217ed9c65b8dcd4ca29
April-cl <xcl8202@gmail.com>
2021-06-24 02:09:37 +0800

    完成导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,91 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const Main = styled.div`
+  flex-grow: 1;
+`
+
+const Nav = styled.nav`
+  border: 1px solid blue;
+  ul {
+    display: flex;
+    li {
+      width: 33.33%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/edit">
+              <Edit />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from='/' to='/edit'></Redirect>
+            <Route path='*'>
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/edit">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
+  );
+}
+
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Edit() {
+  return <h2>记账页</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页</h2>;
+}
+
+function NoMatch() {
+  return (
+    <h2>
+      页面不存在
+    </h2>
   );
 }
 
