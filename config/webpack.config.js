1e935d1a613ece630eddae0de88b3426e79513eb
April-cl <xcl8202@gmail.com>
2021-06-24 13:02:52 +0800

    完成三个icon

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -368,6 +368,15 @@ module.exports = function (webpackEnv) {
           // back to the "file" loader at the end of the loader list.
           oneOf: [
             // TODO: Merge this config once `image/avif` is in the mime-db
+
+            {
+              test: /\.svg$/,
+              use: [
+                { loader: 'svg-sprite-loader', options: {} },
+                { loader: 'svgo-loader', options: {}}
+              ]
+            },
+
             // https://github.com/jshttp/mime-db
             {
               test: [/\.avif$/],
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import React from 'react';
 
+require('icons/edit.svg')
+require('icons/statistics.svg')
+require('icons/tags.svg')
+
 const NavWrapper = styled.nav`
   border: 1px solid blue;
   ul {
@@ -9,7 +13,10 @@ const NavWrapper = styled.nav`
     li {
       width: 33.33%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
     }
   }
 `
@@ -19,12 +26,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref='#tags' />
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref='#edit' />
+          </svg>
           <Link to="/edit">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref='#statistics' />
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
