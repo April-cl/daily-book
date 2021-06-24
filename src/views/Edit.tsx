6c25a7b4c22521137efcb06deabea89be2271a78
April-cl <xcl8202@gmail.com>
2021-06-25 02:09:16 +0800

    完成样式：记账页-收支类型

--- src/views/Edit.tsx
+++ src/views/Edit.tsx
@@ -3,7 +3,40 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CategorySection = styled.section`
-  border: 1px solid yellow;
+  color: #fff;
+  padding: 12px 0;
+  background-color: #26b59a;
+  position: relative;
+  > ul {
+    font-size: 18px;
+    display: flex;
+    justify-content: center;
+    li {
+      padding: 4px 20px;
+      border: 1px solid #fff;
+      cursor: pointer;
+      &:first-child {
+        border-top-left-radius: 4px;
+        border-bottom-left-radius: 4px;
+        border-right: none;
+      }
+      &:last-child {
+        border-top-right-radius: 4px;
+        border-bottom-right-radius: 4px;
+      }
+      &.selected {
+        background-color: #fff;
+        color: #26b59a;
+      }
+    }
+  }
+  > .saveRecord {
+    position: absolute;
+    top: 50%;
+    right: 16px;
+    transform: translateY(-50%);
+    cursor: pointer;
+  }
 `
 const DateSection = styled.section`
   border: 1px solid darkblue;
@@ -24,9 +57,10 @@ function Edit() {
     <Layout>
       <CategorySection>
         <ul>
-          <li>支出</li>
+          <li className='selected'>支出</li>
           <li>收入</li>
         </ul>
+        <button className="saveRecord">保存</button>
       </CategorySection>
       <DateSection>
         <input type="date"/>
